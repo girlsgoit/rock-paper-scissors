a195e3b5849190b0de3530c91a96a07fea6e0c4d
Daniel Verejan <danielverejan@users.noreply.github.com>
2021-03-26 19:07:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Here");
+
+// Crează un joc de rock - paper - scissors
+// Primul jucător vei fi tu, al doilea calculatorul
+
+// Part.1
+// 1. Adăugăm event listener pentru butoane, câte unul pentru fiecare buton
+// 2. Creăm funcția ce ne va returna alegerea calculatorului
+// 3. Comparăm alegerea calculatorului cu a user-ului
+// 4. Anunțăm câștigătorul rundei(cine și ce variantă a ales)
+// 5. Creăm o funcție ce primește ca parametru numărul opțiunei,
+//    și returnează numele opțiunii
+
+// Part.2
+// 1. Variabile pentru număurl total de runde și runda curentă
+// 2. Când se joacă o rundă, să modificăm runda curentă
+// 3. După ultima rundă, anunțăm câștigătorul pe general, cu scorul pentru fiecare
+// 4. Afișăm la ecran progresul rundelor
+// 5. După ultima rundă, resetăm scorul și runda curentă
+
+// Part.3 Optional
+// Afișăm în pagină scorul curent a calculatorului și utilizatorului
+
+// Part.4 Optional
+// O variantă de joc nelimitată în variante
